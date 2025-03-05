89905a87433babdc17684eeab305fd9f5cc675c7
HebaKhamis26 <hebakames3@gmail.com>
2025-03-05 05:32:01 +0200

    add process.env.port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import bootstrap from "./src/app.controller.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 bootstrap(app, express, cors);
 
